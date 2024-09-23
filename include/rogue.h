feef13ee227b1ba0895ee84bc99a08e67a009e11
Glowstudent777 <55334764+Glowstudent777@users.noreply.github.com>
2024-09-23 15:34:30 -0500

    Draw map

--- include/rogue.h
+++ include/rogue.h
@@ -1,14 +1,20 @@
 #ifndef ROGUE_H
 #define ROGUE_H
 
-#include <ncurses/ncurses.h>
+#include <ncurses/curses.h>
 #include <stdlib.h>
 
 typedef struct
 {
-    int x;
     int y;
+    int x;
 } Position;
+
+typedef struct
+{
+    char ch;
+    bool walkable;
+} Tile;
 
 typedef struct
 {
@@ -16,11 +22,30 @@ typedef struct
     char ch;
 } Entity;
 
+// draw.c functions
+void drawMap(void);
+void drawEntity(Entity *entity);
+void drawEverything(void);
+
+// engine.c functions
+void cursesSetup(void);
+void gameLoop(void);
+void closeGame(void);
+
+// map.c functions
+Tile **createMapTiles(void);
+Position setupMap(void);
+void freeMap(void);
+
 // player.c functions
-Entity* createPlayer(Position start_pos);
+Entity *createPlayer(Position start_pos);
 void handleInput(int input);
+void movePlayer(Position newPos);
 
 // externs
-extern Entity* player;
+extern const int MAP_HEIGHT;
+extern const int MAP_WIDTH;
+extern Entity *player;
+extern Tile **map;
 
 #endif
--- src/player.c
+++ src/player.c
@@ -1,8 +1,8 @@
 #include <rogue.h>
 
-Entity* createPlayer(Position start_pos)
+Entity *createPlayer(Position start_pos)
 {
-    Entity* newPlayer = calloc(1, sizeof(Entity));
+    Entity *newPlayer = calloc(1, sizeof(Entity));
 
     newPlayer->pos.y = start_pos.y;
     newPlayer->pos.x = start_pos.x;
@@ -13,21 +13,38 @@ Entity* createPlayer(Position start_pos)
 
 void handleInput(int input)
 {
+    Position newPos = {player->pos.y, player->pos.x};
+
     switch (input)
     {
-        case 'k':
-            player->pos.y--;
-            break;
-        case 'j':
-            player->pos.y++;
-            break;
-        case 'h':
-            player->pos.x--;
-            break;
-        case 'l':
-            player->pos.x++;
-            break;
-        default:
-            break;
+    // move up
+    case 'k':
+        newPos.y--;
+        break;
+    // move down
+    case 'j':
+        newPos.y++;
+        break;
+    // move left
+    case 'h':
+        newPos.x--;
+        break;
+    // move right
+    case 'l':
+        newPos.x++;
+        break;
+    default:
+        break;
+    }
+
+    movePlayer(newPos);
+}
+
+void movePlayer(Position newPos)
+{
+    if (map[newPos.y][newPos.x].walkable)
+    {
+        player->pos.y = newPos.y;
+        player->pos.x = newPos.x;
     }
 }
